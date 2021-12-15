8ba6da2a331d827803ba110ddee721a357436790
ilpo21 <ilarikeronen@gmail.com>
2021-12-15 16:44:24 +0200

    15.12.2021

---
+++ Ohjelmointi1/Tehtävä63.c
@@ -0,0 +1,5 @@
+// Tee ohjelma, joka laskee tekstitiedoston (teksti.txt) rivien ja merkkien lukumäärät ja tulostaa tiedot (alla esimerkkinumerot - ohjelmasi tulee laskea toki varsinaiset arvot ja näyttää ne alla olevien numeroiden sijasta) kuvaruudulle muodossa...
+
+
+// Rivit: 234
+// Merkit: 2334
