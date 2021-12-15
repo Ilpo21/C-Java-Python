3f9fce9455478451278004e72ae00561dfde947e
ilpo21 <ilarikeronen@gmail.com>
2021-12-15 16:44:39 +0200

    15.12.2021

---
+++ Ohjelmointi1/Tehtävä60.c
@@ -0,0 +1,14 @@
+// Tee ohjelma, jossa talletetaan tason pisteiden koordinaatteja (5 kpl) tietuetaulukkoon. Talletetut pisteet kuvaavat toisiinsa liittyviä suoria, ohjelma laskee ja tulostaa suorien muodostaman ketjun pituuden sekä kuinka pitkä matka olisi "linnuntietä" alku- ja loppupisteiden välillä.
+
+// Kahden pisteen välinen etäisyys voidaan laskea kaavalla
+
+
+
+// kaava kahden pisteen välisen etäisyyden laskemiseen
+//etaisyys=(neliöjuuri)(piste1.x-piste2.x)^2+(piste1.y-piste2.y)^2
+
+
+// Muotoile lopputulos seuraavasti:
+
+// Etäisyys mutkitellen on #.###
+// Alku ja loppupisteiden valinen etaisyys on #.###
---
+++ Ohjelmointi1/tehtävä65.c
@@ -0,0 +1,15 @@
+// Kirjoita suomi - englanti atk-sanakirjaohjelma, joka käyttää tiedostoksi tallennettua sanakirjaa. Ohjelma pyytää sanan, joka voi olla suomeksi tai englanniksi. Ohjelma tulostaa sanan vastineen toisella kielellä muodossa
+
+// Sana sana1 englanniksi on word1
+// tai
+
+// Sana word2 suomeksi on sana2
+// Mikäli sanaa ei löydy sanakirjasta, tulostetaan virheilmoitus:
+
+//  Sanaa haettavasana ei loytynyt sanakirjasta
+// Sanat sijaitsevat tiedostossa sanakirja.txt jonka rakenne on seuraavanlainen:
+
+// sana1suomeksi;sana1englanniksi
+// sana2suomeksi;sana2englanniksi
+// ...
+// sanaNsuomeksi;sanaNenglanniksi
