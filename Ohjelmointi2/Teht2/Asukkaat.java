495665ca2b5e717e56adec30ded6672c8fb206c6
ilpo21 <ilarikeronen@gmail.com>
2022-02-20 20:12:32 +0200

    Commit

---
+++ Ohjelmointi2/Teht2/Asukkaat.java
@@ -0,0 +1,33 @@
+public class Asukkaat {
+    private String name;
+    private String bday;
+
+    public Resident(String name, String bday){
+        this.name=name;
+        this.bday=bday;
+    }
+ 
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBDay() {
+        return this.bday;
+    }
+
+    public void setBDay(String bDay) {
+        this.bday = bday;
+    }
+    
+
+    public void print(){
+        System.out.println("Name of resident:"+ name);
+        System.out.println("Birthdate of resident:"+ bday);
+    }
+    
+}
